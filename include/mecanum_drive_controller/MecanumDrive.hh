7ed157d900d672d4087dbd361e49ecd9e423c6db
veroarriola <v.arriola@ciencias.unam.mx>
2025-11-27 18:07:23 -0600

    Following Apache 2.0, I declare I modified the code

--- include/mecanum_drive_controller/MecanumDrive.hh
+++ include/mecanum_drive_controller/MecanumDrive.hh
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
+ * 2025 modifications by @veroarriola
+ *
  */
 #ifndef GZ_SIM_SYSTEMS_MECANUMDRIVE_HH_
 #define GZ_SIM_SYSTEMS_MECANUMDRIVE_HH_
